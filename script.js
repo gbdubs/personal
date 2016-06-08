267137b3a43affee7e03d935daaac98fa905e422
gradyberryward <gward@brandeis.edu>
2016-06-08 15:55:04 -0400

    Update to De Stijl (Chrome Only Until DrawMe)

---
+++ script.js
@@ -0,0 +1,32 @@
+$(function(){
+	var wrapper = $(".title-wrapper").first();
+	$(".menu > .item").each(function(index, element){
+		if ($(element).attr("href")){
+
+		} else {
+			var name = $(element).text().toLowerCase();
+			var corresponding = $("#"+name+"-wrapper");
+			var lines = $("#"+name+"-lines");
+			$(element).click(function(){
+				if ($(element).hasClass("selected")){
+					$(element).removeClass("selected");
+					$(lines).removeClass("selected");
+					$(corresponding).removeClass("selected");
+					$(wrapper).removeClass("menu-open").addClass("menu-opened");
+				} else if ($(wrapper).hasClass("menu-open")){
+					$(".selected").removeClass("selected");
+					$(element).addClass("selected");
+					$(lines).addClass("selected");
+					$(corresponding).addClass("selected");
+				} else {
+					$(element).addClass("selected");
+					$(lines).addClass("selected");
+					$(corresponding).addClass("selected");
+					$(wrapper).addClass("menu-open");
+				}
+			});
+		}
+
+	});
+
+});
